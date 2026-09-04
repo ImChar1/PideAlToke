24d085bea2ee0628975f37e9ba2304ad44750a8b
ImChar1 <soucharuno@gmail.com>
2026-09-04 15:22:44 -0400

    bugfix/ms-pedidos: Se corrije import mal escrito luego de la corrección del nombre de la carpeta infrastructure.

--- desarrollo/backend/ms-pedidos/app/main.py
+++ desarrollo/backend/ms-pedidos/app/main.py
@@ -1,6 +1,6 @@
 # Punto de entrada de la app
 from fastapi import FastAPI
-from app.infraestructure.db.session import Base, engine
+from app.infrastructure.db.session import Base, engine
 from app.adapters.api.v1.routers import pedidos_router
 
 # Crear la tabla en la db si no existe para desarrollo local
